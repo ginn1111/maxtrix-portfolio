982f85f79b0e543543a0e536118c034616c0b350
ginn1111 <vanthuanjw@gmail.com>
2026-08-24 00:41:29 +0700

    fix: keep article toc sticky

--- app/shared/index.tsx
+++ app/shared/index.tsx
@@ -6,7 +6,7 @@ import { Sidebar } from "./sidebar";
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-background text-on-background relative overflow-hidden">
+    <div className="min-h-screen bg-background text-on-background relative overflow-x-clip">
       <Sidebar />
       <Header />
       <main className="md:ml-[60px] lg:ml-64  md:pt-8 py-14 px-margin min-h-screen flex flex-col items-center relative @container">
